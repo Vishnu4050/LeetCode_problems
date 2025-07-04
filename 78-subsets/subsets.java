62b725a81fd9e0f397aea480fd6693c93fcab7cf
Vishnu4050 <venkatavishnu50@gmail.com>
2025-07-04 14:18:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (92.10%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -6,7 +6,6 @@ class Solution {
     }
     public static void backTrack(List<List<Integer>> res,List<Integer> tempSet,int[] nums,int start){
         res.add(new ArrayList<>(tempSet));
-
         for(int i=start;i<nums.length;i++){
             tempSet.add(nums[i]);
             backTrack(res,tempSet,nums,i+1);
