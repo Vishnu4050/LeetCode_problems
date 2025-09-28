aa324ca60a1497e3aac93a5f21481a4fab92e140
Vishnu4050 <venkatavishnu50@gmail.com>
2025-09-28 22:18:02 +0530

    Time: 50 ms (7.27%) | Memory: 44.9 MB (80.26%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -3,28 +3,15 @@ import java.util.Arrays;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
-        int[][] pair = new int[n][2];
-        
-        for (int i = 0; i < n; i++) {
-            pair[i][0] = nums[i];
-            pair[i][1] = i;
-        }
-        
-        Arrays.sort(pair, (a, b) -> Integer.compare(a[0], b[0]));
-        
-        int left = 0, right = n - 1;
-        while (left < right) {
-            int sum = pair[left][0] + pair[right][0];
-            if (sum == target) {
-                return new int[]{pair[left][1], pair[right][1]};
-            } else if (sum < target) {
-                left++;
-            } else {
-                right--;
+        int arr[]=new int[2];
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+            if(nums[i]+nums[j]==target){
+                arr[0]=i;
+                arr[1]=j;
+            }
             }
         }
-        
-        // No solution found - depending on constraints, you can throw an exception
-        throw new IllegalArgumentException("No two sum solution");
+        return arr;
     }
 }
