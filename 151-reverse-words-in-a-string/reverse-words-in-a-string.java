2f9e2de12d942e13c6724857d0d20c6978dd470d
Vishnu4050 <venkatavishnu50@gmail.com>
2025-09-30 12:20:00 +0530

    Time: 9 ms (24.40%) | Memory: 42.4 MB (98.23%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,20 +1,77 @@
 class Solution {
     public String reverseWords(String s) {
-        String res = "";
+        // Step 1: use StringBuilder to reverse the whole string  
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
 
-        for(int i=0;i<s.length();i++)
-        {
-           StringBuilder sb=new StringBuilder();
-           while(i<s.length() && s.charAt(i)!=' ')
-           {
-            sb.append(s.charAt(i));
-            i++;
-           }
-           if(sb.length()!=0)
-           {
-            res=" "+sb+res;
-           }
+        // Step 2: reverse each word in this reversed string
+        reverseEachWord(sb);
+
+        // Step 3: clean up spaces — trim extra spaces, collapse multiple spaces into one
+        return cleanSpaces(sb);
+    }
+
+    // Reverse substrings corresponding to words in sb
+    private void reverseEachWord(StringBuilder sb) {
+        int n = sb.length();
+        int i = 0, j = 0;
+
+        while (i < n) {
+            // skip spaces to find start of a word
+            while (i < n && sb.charAt(i) == ' ') {
+                i++;
+            }
+            // j moves to the end of this word (or to next space)
+            j = i;
+            while (j < n && sb.charAt(j) != ' ') {
+                j++;
+            }
+            // now [i, j-1] is a word, reverse it
+            reverse(sb, i, j - 1);
+
+            // move i forward for next iteration
+            i = j;
         }
-        return res.trim();  
+    }
+
+    // Reverse characters in sb between indices l and r inclusive
+    private void reverse(StringBuilder sb, int l, int r) {
+        while (l < r) {
+            char tmp = sb.charAt(l);
+            sb.setCharAt(l, sb.charAt(r));
+            sb.setCharAt(r, tmp);
+            l++;
+            r--;
+        }
+    }
+
+    // Remove extra spaces (leading, trailing, and multiple in between)
+    private String cleanSpaces(StringBuilder sb) {
+        int n = sb.length();
+        int i = 0, j = 0;
+
+        while (j < n) {
+            // skip spaces
+            while (j < n && sb.charAt(j) == ' ') {
+                j++;
+            }
+            // copy non-space characters
+            while (j < n && sb.charAt(j) != ' ') {
+                sb.setCharAt(i, sb.charAt(j));
+                i++;
+                j++;
+            }
+            // skip spaces again between words
+            while (j < n && sb.charAt(j) == ' ') {
+                j++;
+            }
+            // if there are more words ahead, put a single space
+            if (j < n) {
+                sb.setCharAt(i, ' ');
+                i++;
+            }
+        }
+
+        return sb.substring(0, i);
     }
 }
