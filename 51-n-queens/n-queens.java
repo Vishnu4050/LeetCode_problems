7650cf0dabed6f1167a2849b8d01ae171de25017
Vishnu4050 <venkatavishnu50@gmail.com>
2025-10-19 17:54:13 +0530

    Time: 2 ms (86.15%) | Memory: 44.9 MB (76.74%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -29,21 +29,15 @@ class Solution {
     }
 
     private boolean isSafe(char[][] board, int row, int col, int n) {
-        // Check column
         for (int i = 0; i < row; i++) {
             if (board[i][col] == 'Q') return false;
         }
-
-        // Check upper-left diagonal
         for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
             if (board[i][j] == 'Q') return false;
         }
-
-        // Check upper-right diagonal
         for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
             if (board[i][j] == 'Q') return false;
         }
-
         return true;
     }
 
