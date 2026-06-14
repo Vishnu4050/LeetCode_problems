c2dab4e213bf00d00d5a7253dae28c00aef3d7fc
Peramana Venkat Vishnu <venkatavishnu50@gmail.com>
2026-06-14 12:13:36 +0530

    Time: 10 ms (97.96%) | Memory: 47.4 MB (80.33%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -5,21 +5,26 @@ class Solution {
         for(int num : nums){
             map.put(num,map.getOrDefault(num,0)+1);
         }
-
-        PriorityQueue<Integer> pq =
-                new PriorityQueue<>(
-                    (a,b) -> map.get(b)-map.get(a)
-                );
-
-        pq.addAll(map.keySet());
-
-        int[] ans = new int[k];
-
-        for(int i=0;i<k;i++){
-            ans[i] = pq.poll();
+        List<Integer>[] bucket =new List[nums.length+1];
+        for(int i:map.keySet()){
+            int freq=map.get(i);
+            if(bucket[freq]==null){
+                bucket[freq]=new ArrayList<>();
+            }
+            bucket[freq].add(i);
         }
+        int[] res=new int[k];
+        int counter=0;
+        for(int pos=bucket.length-1;pos >=0 && counter < k;pos--){
+            if(bucket[pos]!=null){
+                for(int i:bucket[pos]){
+                    res[counter++]=i;
+                }
+            }
+        }
+        return res;
 
-        return ans;
+
        
     }
 }
