75a39bc3d82397887d3541372f9229636c8bc001
Peramana Venkat Vishnu <venkatavishnu50@gmail.com>
2026-06-14 17:31:47 +0530

    Time: 3 ms (78.22%) | Memory: 69.8 MB (24.41%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -4,12 +4,11 @@ class Solution {
         for(int i=0;i<k;i++){
             sum+=nums[i];
         }
-        double maxavg=(double)sum/k;
+        int maxsum=sum;
         for(int i=k;i<nums.length;i++){
             sum=sum-nums[i-k]+nums[i];
-            double curavg=(double)sum/k;
-            maxavg=Math.max(maxavg,curavg);
+            maxsum=Math.max(maxsum,sum);
         }
-        return nums.length<=1 ? (double)nums[0]:maxavg;
+        return (double)maxsum/k;
     }
 }
