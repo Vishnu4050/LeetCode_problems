4c6e50bfebe3d53aa9e92da62a129b610a98a4e5
Peramana Venkat Vishnu <venkatavishnu50@gmail.com>
2026-06-16 14:56:39 +0530

    Time: 0 ms (100.00%) | Memory: 47.9 MB (56.61%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,26 +1,24 @@
 class Solution {
     public double myPow(double x, int n) {
-        if(n == 0)return 1.0;
-        if(x== 1)return 1.0;;
-        if(x==0)return 0.0;
-        if(x== -1 && n%2 == 0)return 1.0;
-        if(x== -1 && n%2 != 0)return -1.0;
+         long N = n; 
 
-       long bin=n;
-       if(n < 0){
-        x=1/x;
-        bin = -bin;
-       }
-       double ans=1;
-
-       while(bin>0){
-        if(bin%2 == 1){
-            ans *= x;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
         }
-        x *= x;
-        bin/= 2;
-       }
 
-       return ans; 
+        double ans = 1;
+
+        while (N > 0) {
+
+            if ((N & 1) == 1) {
+                ans *= x;
+            }
+
+            x *= x;
+            N >>= 1;
+        }
+
+        return ans;
     }
 }
